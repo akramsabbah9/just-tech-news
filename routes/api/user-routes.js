3c5c5a6e271bf46db25bddb8ed84fa4bbc2433ff
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-04 02:13:18 -0800

    added login route

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -51,6 +51,31 @@ router.post("/", (req, res) => {
     });
 });
 
+// Login route: /api/users/login
+router.post("/login", (req, res) => {
+    // expects { email, password } in req.body
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: "No user with that email address!" });
+            return;
+        }
+
+        // verify user
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: "Incorrect password!" });
+            return;
+        }
+        
+        res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
+});
+
 // PUT /api/users/1
 router.put("/:id", (req, res) => {
     // expects { username, email, password } in req.body
