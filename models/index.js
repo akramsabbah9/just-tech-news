b76b759603391fa988fb4b0a719186743ce5e109
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-05 17:19:07 -0800

    added comments

--- models/index.js
+++ models/index.js
@@ -1,18 +1,28 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require("./Vote");
+const Comment = require("./Comment");
 
 User.hasMany(Post, {
     foreignKey: "user_id"
 });
 
-User.hasMany(User, {
+User.hasMany(Vote, {
     foreignKey: "user_id"
 });
 
-Post.hasMany(Post, {
+User.hasMany(Comment, {
+    foreignKey: "user_id"
+});
+
+Post.hasMany(Vote, {
     foreignKey: "post_id"
 });
+
+Post.hasMany(Comment, {
+    foreignKey: "post_id"
+});
+
 
 Post.belongsTo(User, {
     foreignKey: "user_id"
@@ -26,6 +36,15 @@ Vote.belongsTo(Post, {
     foreignKey: "post_id"
 });
 
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id"
+});
+
+
 User.belongsToMany(Post, {
     through: Vote,
     as: "voted_posts",
@@ -38,4 +57,4 @@ Post.belongsToMany(User, {
     foreignKey: "post_id"
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
