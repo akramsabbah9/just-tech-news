9a50d087600fedb16127b4983a16241bbc262cc9
Akram Sabbah <akramsabbah9@gmail.com>
2021-03-12 13:50:11 -0800

    added handlebars helper functions and tests

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helpers");
 
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: "Super secret secret",
